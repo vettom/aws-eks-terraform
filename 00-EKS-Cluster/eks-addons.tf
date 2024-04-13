31031ea41a2762defd136d26d8181f4d21afa417
Denny Vettom <dennysv@gmail.com>
2024-04-13 13:13:40 +0100

    Update by DV on Sat 13 Apr 2024 13:13:40 BST

--- 00-EKS-Cluster/eks-addons.tf
+++ 00-EKS-Cluster/eks-addons.tf
@@ -1,18 +1,18 @@
-# resource "aws_eks_addon" "vpc-cni" {
-#   cluster_name  = module.eks.cluster_name
-#   addon_name    = "vpc-cni"
-#   addon_version = "v1.16.2-eksbuild.1"
-#   configuration_values = jsonencode(
-#     {
-#       env = {
-#         ENABLE_POD_ENI                    = "true"  # If using Security group for pods
-#         ENABLE_PREFIX_DELEGATION          = "true"  # Required for ALB to work with target type ip
-#         POD_SECURITY_GROUP_ENFORCING_MODE = "standard"
-#         AWS_VPC_K8S_CNI_EXTERNALSNAT      = "true"  # Optional when using SG for pod outbound traffic routing.
-#         WARM_ENI_TARGET                   = "1"     # optional prefix IP pool.
-#         WARM_PREFIX_TARGET                = "1"
-#       }
-#       enableNetworkPolicy = "true"
-#     }
-#   )
-# }
+resource "aws_eks_addon" "vpc-cni" {
+  cluster_name  = module.eks.cluster_name
+  addon_name    = "vpc-cni"
+  addon_version = "v1.16.2-eksbuild.1"
+  configuration_values = jsonencode(
+    {
+      env = {
+        ENABLE_POD_ENI                    = "true" # If using Security group for pods
+        ENABLE_PREFIX_DELEGATION          = "true" # Required for ALB to work with target type ip
+        POD_SECURITY_GROUP_ENFORCING_MODE = "standard"
+        AWS_VPC_K8S_CNI_EXTERNALSNAT      = "true" # Optional when using SG for pod outbound traffic routing.
+        WARM_ENI_TARGET                   = "1"    # optional prefix IP pool.
+        WARM_PREFIX_TARGET                = "1"
+      }
+      enableNetworkPolicy = "true"
+    }
+  )
+}
--- 00-EKS-Cluster/eks.tf
+++ 00-EKS-Cluster/eks.tf
@@ -1,26 +1,26 @@
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "19.17.4"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.17.4"
 
-#   cluster_name    = "eks-demo"
-#   cluster_version = 1.29
+  cluster_name    = "eks-demo"
+  cluster_version = 1.29
 
-#   cluster_endpoint_public_access = true
-#   enable_irsa                    = true
-#   vpc_id                         = module.vpc.vpc_id
-#   subnet_ids                     = module.vpc.private_subnets
-#   control_plane_subnet_ids       = module.vpc.private_subnets
+  cluster_endpoint_public_access = true
+  enable_irsa                    = true
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
+  control_plane_subnet_ids       = module.vpc.private_subnets
 
-#   eks_managed_node_groups = {
-#     eks_nodegroup_1 = {
-#       min_size       = 1
-#       max_size       = 4
-#       desired_size   = 1
-#       instance_types = ["m5.large"]
-#       capacity_type  = "SPOT"
-#       update_config = {
-#         max_unavailable_percentage = 33
-#       }
-#     }
-#   }
-# }
+  eks_managed_node_groups = {
+    eks_nodegroup_1 = {
+      min_size       = 1
+      max_size       = 4
+      desired_size   = 1
+      instance_types = ["m5.large"]
+      capacity_type  = "SPOT"
+      update_config = {
+        max_unavailable_percentage = 33
+      }
+    }
+  }
+}
