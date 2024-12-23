440a643d1e961bf0bc4fce041463d2c0c6acf6ce
Denny Vettom <dennysv@gmail.com>
2024-12-23 16:25:16 +0000

    Update by DV on Mon 23 Dec 2024 16:25:16 GMT

--- EKS-Cluster-generic/iam-cert-manager.tf
+++ EKS-Cluster-generic/iam-cert-manager.tf
@@ -1,55 +1,55 @@
-# # IAM policy to enable DNS record creation and lookup
-# resource "aws_iam_policy" "certmanager_iam_policy" {
-#   name = "CertManagerIAMPolicy"
-#   policy = jsonencode(
-#     {
-#       "Version" : "2012-10-17",
-#       "Statement" : [
-#         {
-#           "Sid" : "VisualEditor0",
-#           "Effect" : "Allow",
-#           "Action" : "route53:GetChange",
-#           "Resource" : "arn:aws:route53:::change/*"
-#         },
-#         {
-#           "Sid" : "VisualEditor1",
-#           "Effect" : "Allow",
-#           "Action" : [
-#             "route53:ListHostedZones",
-#             "route53:ListHostedZonesByName"
-#           ],
-#           "Resource" : "*"
-#         },
-#         {
-#           "Sid" : "VisualEditor2",
-#           "Effect" : "Allow",
-#           "Action" : "route53:ListResourceRecordSets",
-#           "Resource" : "arn:aws:route53:::hostedzone/*"
-#         },
-#         {
-#           "Sid" : "VisualEditor3",
-#           "Effect" : "Allow",
-#           "Action" : "route53:ChangeResourceRecordSets",
-#           "Resource" : "arn:aws:route53:::hostedzone/*"
-#         }
-#       ]
-#     }
-#   )
-# }
-# # IAM role for cert manager 
-# resource "aws_iam_role" "certmanager_iam_role" {
-#   name               = cert-manager-sa
-#   assume_role_policy = data.aws_iam_policy_document.podidentity.json
-# }
-# # Attach policy to IAM role
-# resource "aws_iam_role_policy_attachment" "certmanager_policy_attachement" {
-#   role       = aws_iam_role.certmanager_iam_role.name
-#   policy_arn = aws_iam_policy.certmanager_iam_policy.arn
-# }
-# # Associate Pod identity with IAM role
-# resource "aws_eks_pod_identity_association" "certmanager" {
-#   cluster_name    = module.eks.cluster_name
-#   namespace       = "cert-manager"
-#   service_account = "cert-manager"
-#   role_arn        = aws_iam_role.certmanager_iam_role.arn
-# }
+# IAM policy to enable DNS record creation and lookup
+resource "aws_iam_policy" "certmanager_iam_policy" {
+  name = "CertManagerIAMPolicy"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "VisualEditor0",
+          "Effect" : "Allow",
+          "Action" : "route53:GetChange",
+          "Resource" : "arn:aws:route53:::change/*"
+        },
+        {
+          "Sid" : "VisualEditor1",
+          "Effect" : "Allow",
+          "Action" : [
+            "route53:ListHostedZones",
+            "route53:ListHostedZonesByName"
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Sid" : "VisualEditor2",
+          "Effect" : "Allow",
+          "Action" : "route53:ListResourceRecordSets",
+          "Resource" : "arn:aws:route53:::hostedzone/*"
+        },
+        {
+          "Sid" : "VisualEditor3",
+          "Effect" : "Allow",
+          "Action" : "route53:ChangeResourceRecordSets",
+          "Resource" : "arn:aws:route53:::hostedzone/*"
+        }
+      ]
+    }
+  )
+}
+# IAM role for cert manager 
+resource "aws_iam_role" "certmanager_iam_role" {
+  name               = "cert-manager-iam-role"
+  assume_role_policy = data.aws_iam_policy_document.podidentity.json
+}
+# Attach policy to IAM role
+resource "aws_iam_role_policy_attachment" "certmanager_policy_attachement" {
+  role       = aws_iam_role.certmanager_iam_role.name
+  policy_arn = aws_iam_policy.certmanager_iam_policy.arn
+}
+# Associate Pod identity with IAM role
+resource "aws_eks_pod_identity_association" "certmanager" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "cert-manager"
+  service_account = "cert-manager"
+  role_arn        = aws_iam_role.certmanager_iam_role.arn
+}
--- EKS-Cluster-generic/iam-ext-dns.tf
+++ EKS-Cluster-generic/iam-ext-dns.tf
@@ -3,12 +3,12 @@ resource "aws_iam_role" "externaldns_iam_role" {
   assume_role_policy = data.aws_iam_policy_document.podidentity.json
 }
 
-# resource "aws_eks_pod_identity_association" "externaldns" {
-#   cluster_name    = module.eks.cluster_name
-#   namespace       = "external-dns"
-#   service_account = "external-dns-controller"
-#   role_arn        = aws_iam_role.externaldns_iam_role.arn
-# }
+resource "aws_eks_pod_identity_association" "externaldns" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = "external-dns"
+  service_account = "external-dns-controller"
+  role_arn        = aws_iam_role.externaldns_iam_role.arn
+}
 
 resource "aws_iam_policy" "external_dns_iam_policy" {
   name = "ExternalDNSControllerIAMPolicy"
