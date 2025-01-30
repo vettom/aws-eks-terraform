02aca8e01e43f77fd65557e9031776f3868528af
Denny Vettom <dennysv@gmail.com>
2025-01-30 20:59:44 +0000

    Update by DV on Thu 30 Jan 2025 20:59:44 GMT

---
+++ EKS-AUTO-generic/data.tf
@@ -0,0 +1,18 @@
+# Trust policy for Pod Identity service
+data "aws_iam_policy_document" "podidentity" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+  }
+}
+# data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
