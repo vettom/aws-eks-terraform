59841403c06eb23ef485da8951e2a465e7b6d9ec
Denny Vettom <dennysv@gmail.com>
2025-06-26 17:13:05 +0100

    Update terraform.tf

--- Auto-EKS-Generic/terraform.tf
+++ Auto-EKS-Generic/terraform.tf
@@ -16,15 +16,17 @@ provider "aws" {
   region  = "eu-west-1"
   profile = "labs" # Configure profile or authentication as required.
 }
-provider "kubernetes" {
-  host                   = aws_eks_cluster.eks-auto-demo.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks-auto-demo.certificate_authority.0.data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "--profile", "labs", "get-token", "--cluster-name", aws_eks_cluster.eks-auto-demo.name]
-    command     = "aws"
-  }
-}
+
+# Disabling as it cannot work during EKS provisioning.
+#provider "kubernetes" {
+#  host                   = aws_eks_cluster.eks-auto-demo.endpoint
+#  cluster_ca_certificate = base64decode(aws_eks_cluster.eks-auto-demo.certificate_authority.0.data)
+#  exec {
+#    api_version = "client.authentication.k8s.io/v1beta1"
+#    args        = ["eks", "--profile", "labs", "get-token", "--cluster-name", aws_eks_cluster.eks-auto-demo.name]
+#    command     = "aws"
+#  }
+#}
 
 
   
